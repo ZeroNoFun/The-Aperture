d3f78e8ed67d509b50b92f8bc11ce9525bc4467d
CrishNate <demidemid1@gmail.com>
2017-01-17 23:30:00 +0200

    Update of bridge

--- lua/entities/prop_wall_projector.lua
+++ lua/entities/prop_wall_projector.lua
@@ -45,8 +45,8 @@ function ENT:Draw()
 	local bridge_draw_width = 35
 	local totalDistance = self:GetPos():Distance(_bridge_trace.HitPos)
 	
-	local bridge_mat = Material("effects/projected_wall", "nocull")
-	local bridge_border_mat = Material("effects/laser1")
+	local mat_bridge = Material("effects/projected_wall")
+	local mat_bridge_border = Material("effects/bluelaser1")
 	
 	if (totalDistance != self.hard_light_bridge_update) then
 		self.hard_light_bridge_update = totalDistance
@@ -54,17 +54,14 @@ function ENT:Draw()
 		local min, max = self:GetRenderBounds() 
 		self:SetRenderBounds(min, max + Vector(totalDistance, 0, 0))
 	end
-	
-	render.SetMaterial(bridge_border_mat)
-	render.DrawBeam(self:LocalToWorld(Vector(0, bridge_draw_width, 0)), self:LocalToWorld(Vector(totalDistance, bridge_draw_width, 0)), 40, 0, 1, Color(100, 200, 255) )
-	render.DrawBeam(self:LocalToWorld(Vector(0, -bridge_draw_width, 0)), self:LocalToWorld(Vector(totalDistance, -bridge_draw_width, 0)), 40, 0, 1, Color(100, 200, 255) )
-	
-	render.SetMaterial(bridge_mat)
-	render.DrawBox(self:LocalToWorld(Vector(totalDistance / 2, 0, 0)), self:LocalToWorldAngles(Angle(0, 0, 0)), -Vector(totalDistance / 2, bridge_draw_width, 0), Vector(totalDistance / 2, bridge_draw_width, 0), Color(255, 255, 255, 255), true) 
-	render.SetColorMaterial()
-	render.DrawBox(self:LocalToWorld(Vector(totalDistance / 2, 0, 0)), self:LocalToWorldAngles(Angle(0, 0, 0)), -Vector(totalDistance / 2, bridge_draw_width, 0), Vector(totalDistance / 2, bridge_draw_width, 0), Color(100, 200, 255, 50), true) 
 
-	
+	local border_width = 10
+	render.SetMaterial(mat_bridge_border)
+	render.DrawBeam(self:LocalToWorld(Vector(0, bridge_draw_width, 0)), self:LocalToWorld(Vector(totalDistance, bridge_draw_width, 0)), border_width, 0, 1, Color(100, 200, 255) )
+	render.DrawBeam(self:LocalToWorld(Vector(0, -bridge_draw_width, 0)), self:LocalToWorld(Vector(totalDistance, -bridge_draw_width, 0)), border_width, 0, 1, Color(100, 200, 255) )
+
+	//render.SetMaterial(mat_bridge)
+	//render.DrawBox(self:LocalToWorld(Vector(totalDistance / 2, 0, 0)), self:LocalToWorldAngles(Angle(0, 0, 0)), -Vector(totalDistance / 2, bridge_draw_width, 0), Vector(totalDistance / 2, bridge_draw_width, 0), Color(255, 255, 255, 255), true)
 end
 
 function ENT:Think()
@@ -74,7 +71,7 @@ function ENT:Think()
 	if SERVER then
 		
 		// const base plate length
-		local plate_length = 47.450005
+		local plate_length = 50.393715
 		
 		local bridge_trace = util.TraceLine( {
 			start = self:GetPos(),
@@ -92,18 +89,18 @@ function ENT:Think()
 				if (v:IsValid()) then v:Remove() end
 			end
 
-			local plateIndex = math.Min(8, math.ceil(totalDistance / plate_length))
-			local addingDist = plate_length * plateIndex / 2
+			local plateIndex = 1 //math.Min(8, math.ceil(totalDistance / plate_length))
+			local addingDist = 0
 			
 			while (totalDistance > 0) do
 				
 				local ent = ents.Create("prop_physics")
-				ent:SetModel("models/hunter/plates/plate1x"..tostring(plateIndex)..".mdl")
+				ent:SetModel("models/wall_projector_bridge/wall.mdl")
 				ent:SetPos(self:LocalToWorld(Vector(addingDist, 0, 0)))
-				ent:SetAngles(self:LocalToWorldAngles(Angle(0, 90, 0)))
+				ent:SetAngles(self:LocalToWorldAngles(Angle(0, 0, 0)))
 				ent:Spawn()
-				ent:SetColor(Color(0, 0, 0, 0))
-				ent:SetRenderMode( RENDERMODE_TRANSALPHA )
+				//ent:SetColor(Color(0, 0, 0, 0))
+				//ent:SetRenderMode( RENDERMODE_TRANSALPHA )
 				ent:DrawShadow(false)
 				ent:SetParent(self)
 
@@ -113,7 +110,7 @@ function ENT:Think()
 				
 				table.insert(self.hard_light_bridges_ents, table.Count(self.hard_light_bridges_ents) + 1, ent)
 
-				plateIndex = math.Min(8, math.ceil(totalDistance / plate_length))
+				plateIndex = 1//math.Min(8, math.ceil(totalDistance / plate_length))
 				addingDist = addingDist + plate_length * plateIndex
 				totalDistance = totalDistance - plate_length * plateIndex
 				
